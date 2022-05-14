e1661a07fc5ef446caabec5815c7488933022d99
Soban <sobanfarooq3@gmail.com>
2022-05-14 15:40:04 +0500

    Add success resource

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SuccessResource;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -13,8 +15,14 @@ class PostController extends Controller
 
     public function storePosts(Request $request) {
         $request->validate([
-            ''
+            'website_id' => 'required|exists:websites,id',
+            'title' => 'required|max:255',
+            'text' => 'required',
         ]);
+
+        Post::create($request);
+
+        return new SuccessResource();
     }
 
     public function getPost() {
--- routes/api.php
+++ routes/api.php
@@ -16,18 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix()->group('/websites', function() {
-    Route::get('/', [WebsiteController::class, 'getWebsites']);
-    Route::post('/', [WebsiteController::class, 'storeWebsites']);
-    Route::put('/show', [WebsiteController::class, 'getWebsites']);
-    Route::put('/update', [WebsiteController::class, 'updateWebsites']);
-    Route::delete('/delete', [WebsiteController::class, 'deleteWebsites']);
-});
+// Route::prefix()->group('/websites', function() {
+//     Route::get('/', [WebsiteController::class, 'getWebsites']);
+//     Route::post('/', [WebsiteController::class, 'storeWebsites']);
+//     Route::put('/show', [WebsiteController::class, 'getWebsites']);
+//     Route::put('/update', [WebsiteController::class, 'updateWebsites']);
+//     Route::delete('/delete', [WebsiteController::class, 'deleteWebsites']);
+// });
 
-Route::prefix()->group('/posts', function() {
-    Route::get('/', [PostController::class, 'getPosts']);
-    Route::post('/', [PostController::class, 'storePosts']);
-    Route::put('/show', [PostController::class, 'getPost']);
-    Route::put('/update', [PostController::class, 'updatePosts']);
-    Route::delete('/delete', [PostController::class, 'deletePosts']);
-});
+// Route::prefix()->group('/posts', function() {
+    // Route::get('/', [PostController::class, 'getPosts']);
+    // Route::post('/', [PostController::class, 'storePosts']);
+    // Route::put('/show', [PostController::class, 'getPost']);
+    // Route::put('/update', [PostController::class, 'updatePosts']);
+    // Route::delete('/delete', [PostController::class, 'deletePosts']);
+// });
